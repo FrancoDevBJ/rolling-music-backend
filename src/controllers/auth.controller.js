35ebbc282bf984d6792c8a8d6a85302bb7482623
Franco Quinteros <fquinteros701@gmail.com>
2026-04-22 03:33:27 -0300

    se agrego profilePic a la respuesta en login del controlador de autenticacion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -159,7 +159,8 @@ const login = async (req, res) => {
                 id: user._id,
                 name: user.name,
                 email: user.email,
-                role: user.role
+                role: user.role,
+                profilePic: user.profilePic
             }
         });
 
